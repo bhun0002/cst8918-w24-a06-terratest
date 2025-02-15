db65bbd3340fbd9f0b5de50adf3c98498b66b865
bhun0002 <bhungaliyadeval@gmail.com>
2025-02-14 22:09:23 -0500

    Commit

--- test/test_vm_image_test.go
+++ test/test_vm_image_test.go
@@ -1,33 +1,37 @@
-// test_vm_image.go
 package test
 
 import (
 	"testing"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/ssh"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAzureVMImage(t *testing.T) {
+func TestAzureVMUbuntuVersion(t *testing.T) {
+	t.Parallel()
+
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
 	}
 
+	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Fetch Terraform outputs
-	vmName := terraform.Output(t, terraformOptions, "vm_name")
-	resourceGroup := terraform.Output(t, terraformOptions, "resource_group_name")
+	// Get the VM Public IP from Terraform output
+	publicIP := terraform.Output(t, terraformOptions, "public_ip")
 
-	// Ensure VM and resource group are not empty
-	assert.NotEmpty(t, vmName, "VM name should not be empty")
-	assert.NotEmpty(t, resourceGroup, "Resource group should not be empty")
+	// Define the SSH key and user
+	sshUser := "azureadmin"
+	sshKey := "~/.ssh/id_rsa"
 
-	// Expected values from Terraform configuration
-	expectedPublisher := "Canonical"
-	expectedSKU := "22_04-lts-gen2"
+	sshHost := ssh.Host{
+		Hostname:    publicIP,
+		SshKeyPair:  &ssh.KeyPair{PrivateKeyPath: sshKey},
+		SshUserName: sshUser,
+	}
 
-	// Validate the expected image details
-	assert.Equal(t, expectedPublisher, "Canonical", "VM should be from Canonical")
-	assert.Equal(t, expectedSKU, "22_04-lts-gen2", "VM should be running Ubuntu 22.04 LTS")
+	// Run the command to check Ubuntu version
+	ubuntuVersion, err := ssh.CheckSshCommandE(t, sshHost, "lsb_release -d | awk -F':' '{print $2}'")
+	assert.NoError(t, err)
+	assert.Contains(t, ubuntuVersion, "Ubuntu 22.04", "Expected Ubuntu 22.04 LTS")
 }
